7ed17ef3175f2349bf2291db231ff05e712d35f4
unaismuhammad <unaismuhammad477@gmail.com>
2025-10-17 09:32:22 +0530

    updated

--- my module/main.py
+++ my module/main.py
@@ -1,8 +1,9 @@
-import math
-def square_root(n):
-    return math.sqrt(n)
-def power(base,exp):
-    return math.pow(base,exp)
-def ca(r):
-    return math.pi *math.pow(r,2)
+# import math
+# def square_root(n):
+#     return math.sqrt(n)
+# def power(base,exp):
+#     return math.pow(base,exp)
+# def ca(r):
+#     return math.pi *math.pow(r,2)
    
+print("hello world")
